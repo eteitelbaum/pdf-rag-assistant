ce58f42eb301f0b6f90c9c26788e3fcc84e941e5
Emmanuel Teitelbaum <emmanuel.teitelbaum@gmail.com>
2025-01-31 18:54:23 -0500

    fix __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -7,6 +7,6 @@ A modular system for analyzing academic papers using RAG.
 from .models.llm_router import LLMRouter
 from .document_processing.pdf_loader import PDFProcessor
 from .document_processing.vectorstore import VectorStoreManager
-from .utils.metrics_tracker import ResourceTracker
+from .utils.metrics_tracker import MetricsTracker
 
 __all__ = ['LLMRouter', 'PDFProcessor', 'VectorStoreManager', 'ResourceTracker']
